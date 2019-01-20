ffa463004e331d292fea88af967437823e1badd1
GabrM <mahmoudgabr.932@gmail.com>
2019-01-20 17:52:47 +0200

    commit bioFragment view model

--- app/src/main/java/com/example/gabrm/retrofitjsonexample/viewmodel/BioFragmentViewModel.java
+++ app/src/main/java/com/example/gabrm/retrofitjsonexample/viewmodel/BioFragmentViewModel.java
@@ -1,8 +1,19 @@
 package com.example.gabrm.retrofitjsonexample.viewmodel;
 
 import android.arch.lifecycle.ViewModel;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.example.gabrm.retrofitjsonexample.model.HeroModel;
+import com.example.gabrm.retrofitjsonexample.view.BioFragment;
 
 
 public class BioFragmentViewModel extends ViewModel {
-        //TODO
+
+
+    public HeroModel getBundle(HeroModel heroModel, String BUNDLE_TAG, Fragment fragment)
+    {
+        heroModel= (HeroModel) fragment.getArguments().getSerializable(BUNDLE_TAG);
+        return heroModel;
+    }
 }
