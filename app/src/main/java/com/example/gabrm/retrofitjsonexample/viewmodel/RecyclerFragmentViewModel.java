2c24d712b46c38b9c1bd45c48adde5540b5dd27c
GabrM <mahmoudgabr.932@gmail.com>
2019-01-20 15:40:51 +0200

    Hero App v3

---
+++ app/src/main/java/com/example/gabrm/retrofitjsonexample/viewmodel/RecyclerFragmentViewModel.java
@@ -0,0 +1,10 @@
+package com.example.gabrm.retrofitjsonexample.viewmodel;
+
+import android.os.Bundle;
+
+import com.example.gabrm.retrofitjsonexample.model.HeroModel;
+import com.example.gabrm.retrofitjsonexample.view.BioFragment;
+
+public class RecyclerFragmentViewModel extends HeroViewModel {
+
+}
